6fc1f6aa73c82e50696a1776873d6db5332273d4
Alex Innes <alexnixoninnes@gmail.com>
2019-07-03 09:44:22 +0100

    Added eventlogs search

--- New DCDIAG.ps1
+++ New DCDIAG.ps1
@@ -1,4 +1,4 @@
-function get-DCDiagResults {
+function Get-DCDiagResults {
     [cmdletbinding()]
     param(
         # Parameter help description
@@ -31,14 +31,15 @@ function get-DCDiagResults {
         If ($null -ne $TestName -And $null -ne $TestStatus)
         {
             $Results | Add-Member -Name $("$TestName".Trim()) -Value $TestStatus -Type NoteProperty -force
-            $TestName = $Null; $TestStatus = $Null
+            $TestName = $Null
+            $TestStatus = $Null
         }
     }
     $AllDCDiags += $Results
     $AllDCDiags
 }
 
-function get-RepAdminResults  {
+function Get-RepAdminResults  {
     [cmdletbinding()]
     param(
 
@@ -47,25 +48,109 @@ function get-RepAdminResults  {
     )
     $repadmin = @()
     $rep = (Invoke-Command $Server -ScriptBlock{repadmin /showrepl /repsto /csv | ConvertFrom-Csv})
+    $serverOS = Get-WmiObject win32_operatingsystem -ComputerName $server
+
 
     ForEach($r in $rep) {
-    # Adding properties to object
-    $REPObject = New-Object PSCustomObject -Property @{
-        Destination_DC = $r."destination dc"
-        Source_DC = $r."source dc"
-        Source_DC_Site = $r."Source DC Site"
-        Last_Success_Time = $r."last success time"
-        Last_Failure_Status = $r."Last Failure Status"
-        Last_Failure_Time = $r."last failure time"
-        Number_of_failures = $r."number of failures"
+        # Adding properties to object
+        $REPObject = New-Object PSCustomObject -Property @{
+            Last_Success_Time = $r."last success time"
+            Last_Failure_Status = $r."Last Failure Status"
+            Last_Failure_Time = $r."last failure time"
+            Number_of_failures = $r."number of failures"
+        }
+        if(($serverOS).caption -like "*2008*"){
+            $REPObject | Add-Member -Name "Destination DSA Site" -MemberType NoteProperty -Value $r."Destination DSA Site"
+            $REPObject | Add-Member -Name "Destination DSA" -MemberType NoteProperty -Value $r."Destination DSA"
+            $REPObject | Add-Member -Name "Source DSA Site" -MemberType NoteProperty -Value $r."Source DSA Site"
+            $REPObject | Add-Member -Name "Source DSA" -MemberType NoteProperty -Value $r."Source DSA"
 
-    }
 
-    # Adding object to array
-    $repadmin += $REPObject
+        }else{
+            $REPObject | Add-Member -Name "Destination DS Site" -MemberType NoteProperty -Value $r."Destination DS Site"
+            $REPObject | Add-Member -Name "Destination DS" -MemberType NoteProperty -Value $r."Destination DS"
+            $REPObject | Add-Member -Name "Source DS Site" -MemberType NoteProperty -Value $r."Source DS Site"
+            $REPObject | Add-Member -Name "Source DS" -MemberType NoteProperty -Value $r."Source DS"
+        }
 
-    }
+
+
+
+        # Adding object to array
+        $repadmin += $REPObject
+        }
+
+
+
     $repadmin
 }
 
+
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    Example of how to use this cmdlet
+.EXAMPLE
+    Another example of how to use this cmdlet
+#>
+function Check-ReplicationMethod {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false)]
+        $server = "localhost"
+    )
+    $ntfrsService = Get-Service ntfrs -ComputerName $server
+    $dfsService = Get-Service dfsr -ComputerName $server
+
+    If(($ntfrsService).Status -eq "Running"){
+        $replicationMethod = "ntfrs"
+    }ElseIf(($dfsService).Status -eq "Running"){
+        $replicationMethod = "dfsr"
+    }else{
+        Write-Error -Message "neither NtFrs or DFSR are running" -ErrorAction Stop
+    }
+
+    return $replicationMethod
+}
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    Example of how to use this cmdlet
+.EXAMPLE
+    Another example of how to use this cmdlet
+#>
+function Get-DomainAlert {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [validateset("ntfrs", "dfs")]
+        $replicationMethod
+    )
+
+    $Yesterday = (Get-Date) - (New-TimeSpan -Day 1)
+
+    If($replicationMethod -eq "ntfrs"){
+        $logName = "File Replication Service"
+    }else{
+        $logName = "DFS Replication"
+    }
+    $eventHash = @{
+        LogName = $logName
+        StartTime = $Yesterday
+
+    }
+    #search on hash table
+    $events = Get-WinEvent -LogName $logName -ComputerName $computerName -FilterHashtable $eventHash
+    $events
+
+}
 
